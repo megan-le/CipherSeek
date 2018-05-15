0390c77c03dc1d07f8e61d0372a2877e6ede45ed
AbhiDhir <dhir.abhimanyu@gmail.com>
2018-05-14 19:39:43 -0500

    Added Xor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "affine.h"
+#include "xor1.h"
 
 int main() {
   std::cout << affine("This is an Affine Cipher",3,8,true) << "\n";
-  std::cout << affine("Ndgk gk iv Ixxgvu Ogbduh",3,8,false) << "\n"; 
+  std::cout << affine("Ndgk gk iv Ixxgvu Ogbduh",3,8,false) << "\n";
+  std::cout << xor1("This is an XOR Cipher", "test") << "\n";
+  std::cout << xor1(xor1("This is an XOR Cipher", "test"),"test") << "\n"; 
 }
