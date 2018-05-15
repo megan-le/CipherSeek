6d232c1457017f240b686fa03e05151cd90c272a
AbhiDhir <dhir.abhimanyu@gmail.com>
2018-05-14 19:27:35 -0500

    Added Affine as Header File

--- affine.cpp
+++ affine.cpp
@@ -1,31 +1,4 @@
-#include <iostream>
-#include<string>
-
-using namespace std;
-
-string caesar(string a, int shift, bool encrypt){
-  string answer = "";
-  for(int i = 0; i<a.length(); i++){
-    int val = 0;
-    if(a[i] == ' '){
-      answer += " ";
-    }else if(!isalpha(a[i])){
-      answer+= a[i];
-    }else if(isupper(a[i])){
-      val = 65;
-    }else{
-      val = 97;
-    }
-    if(val != 0){
-      if(encrypt){
-        answer+=char((((int(a[i])-val+shift)%26)+26)%26+val);
-      }else{
-        answer+=char((((int(a[i])-val-shift)%26)+26)%26+val);
-      }
-    }
-  }
-  return answer;
-}
+#include "affine.h"
 
 string affine(string m, int a, int b, bool encrypt){
   string answer = "";
